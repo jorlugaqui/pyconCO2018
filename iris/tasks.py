eff1b60c5ba9b5860660872900e24edeb396beee
jorge.galvis <jorge.galvis@globant.com>
2018-02-08 16:37:55 -0500

    Refactor tasks

--- iris/tasks.py
+++ iris/tasks.py
@@ -9,19 +9,15 @@ logger = logging.getLogger(__name__)
 @task
 def classify_iris(pk):
     """Call the model and predict classification"""
-    result = None
-
     try:
         result = Result.objects.get(pk=pk)
+        iris_model = IrisModel.get_instance()
+        if iris_model is None:
+            raise Exception('Predictions are not available')
+        prediction = iris_model.predict([[result.petal_length, result.petal_width]])
+        result.classification = IrisModelConfig.CLASSIFICATION.get(prediction[0])
+        result.save()
     except Result.DoesNotExist:
         logger.exception('No result record found in the DB')
-        return
-
-    iris_model = IrisModel.get_instance()
-
-    if iris_model is None:
-        raise Exception('Predictions are not available')
-
-    prediction = iris_model.predict([[result.petal_length, result.petal_width]])
-    result.classification = IrisModelConfig.CLASSIFICATION.get(prediction[0])
-    result.save()
+    except Exception as e:
+        logger.exception(e.message)
